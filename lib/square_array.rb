5c62dd40e80e63921c221b4c6854bb1a09bb2e48
Diana Liao <dijali@gmail.com>
2021-01-15 18:42:35 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
+
+
 def square_array(array)
-  # your code here
+ squares=[]
+ array.length.times do |n|
+   squares << array[n]**2 
+ end
+ squares
 end
+
+#def square_array(array)
+ #squares = []
+ #counter = 0  
+  #while array[counter] do
+    #squares << array[counter]**2 
+    #counter += 1
+  #end 
+  #squares
+#end
