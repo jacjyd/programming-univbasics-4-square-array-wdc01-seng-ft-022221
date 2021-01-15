0b5250fad9352e1ec8dcc4f677f6f3a20c4fc0e4
Diana Liao <dijali@gmail.com>
2021-01-15 18:41:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,21 +3,15 @@ require 'pry'
 
 
 def square_array(array)
- squares = []
- counter = 0  
-  while array[counter] do
-    squares << array[counter]**2 
-    counter += 1
-  end 
-  squares
+ 
 end
 
-def square_array(array)
- squares = []
- counter = 0  
-  while array[counter] do
-    squares << array[counter]**2 
-    counter += 1
-  end 
-  squares
-end
+#def square_array(array)
+ #squares = []
+ #counter = 0  
+  #while array[counter] do
+    #squares << array[counter]**2 
+    #counter += 1
+  #end 
+  #squares
+#end
